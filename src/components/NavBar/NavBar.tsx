d7073cba8602c451cd2dd26dab1892001d602c2e
Rfidelis <ricardinhofidelis@hotmail.com>
2025-10-25 11:52:34 -0300

    fix: StyledToobar in NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,9 +3,13 @@ import { AppBar, MenuItem, Toolbar } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 const NavBar = () => {
-  const StyledToobar = styled(Toolbar)(({}) => ({
+  const StyledToobar = styled(Toolbar)(({ theme }) => ({
     display: "flex",
     justifyContent: "space-evenly",
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.primary.contrastText,
+    borderBottom: `2px solid ${theme.palette.secondary.main}`,
+    boxShadow: theme.shadows[4],
   }));
 
   return (
